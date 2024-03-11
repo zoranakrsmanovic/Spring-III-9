b6a5b111fcb0db0c1f3026f6bf3463a66a73758b
Zorana Krsmanovic <zorana.krsmanovic@outlook.com>
2024-03-11 13:54:49 +0100

    Initial commit

---
+++ src/main/java/com/prvaapp123/prvaapp123/Prvaapp123Application.java
@@ -0,0 +1,13 @@
+package com.prvaapp123.prvaapp123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prvaapp123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prvaapp123Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/prvaapp123/prvaapp123/Prvaapp123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prvaapp123.prvaapp123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prvaapp123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
